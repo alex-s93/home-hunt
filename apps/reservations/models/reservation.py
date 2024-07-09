91adeafec955d3b9158e360debcb76747d31a8db
alex-s93 <solodkov.alex@gmail.com>
2024-07-09 13:14:34 +0200

    Add Reservation model

---
+++ apps/reservations/models/reservation.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Reservation(models.Model):
+    renter = models.ForeignKey('User', on_delete=models.CASCADE, related_name='reservations')
+    apartment = models.ForeignKey('Apartment', on_delete=models.CASCADE, related_name='reservations')
+    date_from = models.DateField()
+    date_to = models.DateField()
+    is_approved_by_landlord = models.BooleanField(default=False)
+    is_canceled = models.BooleanField(default=False)
